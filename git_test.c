2fa37845452c5ca60264411b1d9b348b6545ab0b
huangjin <286019421@qq.com>
2018-03-27 22:49:13 +0800

    Add a new feature of add
    - add
    
    Issue:
    Typle:

--- git_test.c
+++ git_test.c
@@ -2,9 +2,14 @@ void main()
 {
     int i,j,k;
     i = 10;
-    k = 11;
+    k = 0;
     j = 100;
-    if(i>0);
-    printf("This is a test.\n");
-
+    for(i=1;i<j;i++);
+    {
+    printf("%d+",i);
+    k = k + i;
+    } 
+    k = k + j;    
+    printf("%d=%d",j,k);    
+    printf("%d+...%d=%d\n",i,j,k);
 }
